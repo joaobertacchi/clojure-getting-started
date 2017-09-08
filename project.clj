89ea7baa87c923056913ce609be79e448fb8bae1
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2017-09-08 18:30:41 -0300

    Use Redis for storing input param

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]
-                 [camel-snake-kebab "0.2.4"]]
+                 [camel-snake-kebab "0.2.4"]
+                 [com.taoensso/carmine "2.16.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -5,27 +5,46 @@
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]
-            [camel-snake-kebab.core :as kebab]))
+            [camel-snake-kebab.core :as kebab]
+            [taoensso.carmine :as car :refer (wcar)]))
 
 (def sample (env :sample "sample-string-thing"))
 
+(def server1-conn {:pool {} :spec {:uri (env :redis-url)}}) ; See `wcar` docstring for opts
+(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
+
+(defn record [input]
+  (wcar* 
+    (car/lpush :sayings input)))
+
+(defn samples []
+  (wcar* 
+    (car/lrange :sayings 0 -1)))
+
 (defn splash []
   {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body (for [kind ["camel" "snake" "kebab"]]
-            (format "<a href=\"/%s?input=%s\">%s %s</a><br />"
-              kind sample kind sample))})
+   :headers {"Content-Type" "text/html"}
+   :body (concat (for [kind ["camel" "snake" "kebab"]]
+   (format "<a href=\"/%s?input=%s\">%s %s</a><br />"
+           kind sample kind sample))
+ ["<hr /><ul>"]
+ (for [s (samples)]
+   (format "<li>%s</li>" s))
+ ["</ul>"])})
 
 (defroutes app
   (GET "/camel" {{input :input} :params}
+    (record input)
        {:status 200
         :headers {"Content-Type" "text/plain"}
         :body (kebab/->CamelCase input)})
   (GET "/snake" {{input :input} :params}
+    (record input)
        {:status 200
         :headers {"Content-Type" "text/plain"}
         :body (kebab/->snake_case input)})
   (GET "/kebab" {{input :input} :params}
+    (record input)
        {:status 200
         :headers {"Content-Type" "text/plain"}
         :body (kebab/->kebab-case input)})
